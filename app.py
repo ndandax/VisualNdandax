8aa7ab488e97bb02a9b0eadf103873dcb25dc2c5
ndandax <sebandandala99@gmail.com>
2023-08-28 09:09:02 +0300

    change4

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 import base64
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 engine_id = "stable-diffusion-v1-5"
 api_host = os.getenv('API_HOST', 'https://api.stability.ai')

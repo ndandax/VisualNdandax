7a06a6049a77cd9775734707ef1cd457235ca80c
ndandax <sebandandala99@gmail.com>
2023-08-29 12:39:05 +0300

    change6

--- static/jquery/myjquery.js
+++ static/jquery/myjquery.js
@@ -1,17 +1,58 @@
 $(document).ready(function() {
-    $("html").css("background-color", "#000000");
+    $("html").css("background-color", "#000010");
     $("body").css("color", "#FFFFFF");
-    $(".title1").css("font-family", "Roboto, sans-serif")
-
+    $(".title1").css("font-family", "Roboto, sans-serif");
+     
     const $textPrompt = $("#text-prompt");
-    const minHeight = 30;
-    const maxHeight = 100;
+    const $generateButton = $("#generate-button");
 
+    // Set the initial height and the minimum height
+    const minHeight = 40; // Adjust as needed
+
+    // Update the input's height as new lines are added
     $textPrompt.on("input", function() {
-        const inputHeight = Math.min(maxHeight, Math.max(minHeight, this.value.length * 10));
-        const scrollHeight = this.scrollHeight; // Get the scroll height of the content
+        // Temporarily set height to auto to calculate the scroll height
+        $textPrompt.css("height", "auto");
 
-        // Set both the height and scrollHeight of the input box
-        $(this).height(inputHeight).css("overflow-y", scrollHeight > inputHeight ? "scroll" : "hidden");
+        // Get the scroll height of the content
+        const scrollHeight = this.scrollHeight;
+
+        // Reset the height to its default
+        $textPrompt.css("height", minHeight + "px");
+
+        // Update the input's height
+        if (scrollHeight > minHeight) {
+            $textPrompt.css("height", scrollHeight + "px");
+        }
     });
+
+    // Function to update button text and disable/enable during generation
+    function updateButtonState(isGenerating) {
+        if (isGenerating) {
+            $generateButton.prop("disabled", true);
+            $generateButton.text("Generating, please wait...");
+            $generateButton.css("background-color", "green"); // Change button color to green
+        } else {
+            $generateButton.prop("disabled", false);
+            $generateButton.text("Generate Image");
+            $generateButton.css("background-color", ""); // Reset button color
+        }
+    }
+
+    $("#generate-button").click(function() {
+        updateButtonState(true); // Change button text and disable
+        // Your image generation logic here
+
+        // Simulate a delay (replace with actual image generation)
+        setTimeout(function() {
+            updateButtonState(false); // Restore button text and enable
+            // Handle the image response
+        }, 3000); // Replace 3000 with the actual time it takes to generate the image
+    });
+      
 });
+
+//$("#text-prompt").on("input", function() {
+  //  $(this).css("height", Math.min($(this).prop("scrollHeight"), 100) + "px");
+  //});
+  
